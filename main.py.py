a160c4242103f6ab09173a8cbec99c0eafb5cfe8
OanaBere <oana.bere@stud.ubbcluj.ro>
2021-10-13 16:11:16 +0300

    final

--- main.py.py
+++ main.py.py
@@ -4,10 +4,14 @@ def printMenu():
     print ('2. Verificarea conjecturii lui Goldbach')
     print ('3. Dati o valoare si un numar de pasi confrom cerintei 4 ')
     print ('4. Radicalul unui numar folosind metoda lui Newton (afisarea aproximarii)')
-    print ('5. Iesire')
+    print ('5. Dati valoare pentru verificare numar palindrom ')
+    print ('6. Verificare daca un numar este palindrom sau nu conform cerintei 5')
+    print ('7. Iesire')
 
 def verificare_prim(a):
-
+    '''
+    functie care verifica daca numarul e prim
+    '''
     if a < 1:
         return False
     if a == 2:
@@ -22,6 +26,7 @@ def verificare_prim(a):
 
 def get_goldbach(n):
     """
+    functie care determina doua numere prime, un minim si un maxim, a caror suma este egala cu n
     :param n: numar natural
     :return: doua numere prime, un minim si un maxim, a caror suma este egala cu n
     """
@@ -32,10 +37,11 @@ def get_goldbach(n):
             if verificare_prim(b):
                 return a, b
 
-# functie care returneaza aproximarea radacinei unui numar folosind metoda Newton dupa un anumit numar de pasi
+
 
 def get_newton_sqrt(n, steps):
     """
+    functie care returneaza aproximarea radacinei unui numar folosind metoda Newton dupa un anumit numar de pasi
     :param n: numarul dat
     :param steps:numarul de pasi
     :return:float
@@ -72,12 +78,35 @@ def test_get_newton_sqrt():
 
 test_get_newton_sqrt()
 
+
+def is_palindrome (n):
+    '''
+    functie care verifica daca numarul e palindrom
+    :param n: numarul natural
+    :return: bool
+    '''
+    copie = n
+    invers = 0
+    while copie != 0:
+        invers = invers * 10 + copie % 10
+        copie = copie // 10
+
+    if n != invers:
+        return False
+    return True
+
+def test_is_palindrome ():
+    assert is_palindrome (121) is True
+    assert is_palindrome (223) is False
+    assert is_palindrome (424) is True
+
+
 def main():
     while True:
         printMenu()
         optiune = input('Alegeti optiunea: ')
         if optiune == "1":
-            n= int(input("n = "))
+            n = int(input("n = "))
         elif optiune == "2":
             print(get_goldbach(n))
         elif optiune == "3":
@@ -86,6 +115,10 @@ def main():
         elif optiune == "4":
             print(get_newton_sqrt(n, steps))
         elif optiune == "5":
+            n = int(input("n = "))
+        elif optiune == "6":
+            print(is_palindrome(n))
+        elif optiune == "7":
             break
 
 main()
